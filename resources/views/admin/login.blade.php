cc98bc22f7b5f6c0e7c57d188aa1667e74d30d19
PotsonHumer <potsonhumer@gmail.com>
2017-07-14 20:45:41 +0800

    修改後台登入畫面樣式

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.admin')
 
 @section('content')
-<div id="loginForm" class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
-	<form action="{{ url('/admin/login') }}" method="post">
+<div id="loginForm" class="flex flex-middle col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
+	<form action="{{ url('/admin/login') }}" method="post" class="clear">
 
-		<div id="logo" class="flex flex-center"><img src="{{ URL::asset('/images/logo_b.svg') }}" class="img-responsive"></div>
+		<!-- <div id="logo" class="flex flex-center"><img src="{{ URL::asset('/images/logo_b.svg') }}" class="img-responsive"></div> -->
 
 		<h1 class="text-center">歐格思雲端 <small>後台管理系統登入</small></h1>
 
