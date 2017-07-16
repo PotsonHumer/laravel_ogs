6cdb704bea3fdf1d6759c4d00033338eabb451ef
PotsonHumer <potsonhumer@gmail.com>
2017-07-16 22:51:40 +0800

    增加 domain 資料表

---
+++ database/migrations/2017_07_15_064132_create_domain_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomainTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    private $tableName = 'domain';
+
+    public function up()
+    {
+        $dbPrefix = Config::get('database.connections.mysql.prefix');
+
+        Schema::create($this->tableName, function(Blueprint $table){
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('siteid')->index()->nullable()->default(NULL)->comment('站點ID');
+            $table->foreign('siteid')->references('id')->on('site')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('protocol')->nullable()->default(NULL)->comment('連線協定');
+            $table->string('name')->nullable()->default(NULL)->comment('綁定 Domain name');
+
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE `$dbPrefix$this->tableName` comment '站點對應 Domain 資料表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            $table->dropForeign(['siteid']);
+            $table->dropIfExists($this->tableName);
+        });
+    }
+}
