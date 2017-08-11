c4b36331dcf0278a5765bc6ce07cf58ea7a9cd43
PotsonHumer <potsonhumer@gmail.com>
2017-08-11 15:40:12 +0000

    修改後台增加站點表單

--- resources/views/admin/platform/site_add.blade.php
+++ resources/views/admin/platform/site_add.blade.php
@@ -9,20 +9,29 @@
 			<div class="form-group">
 				<div class="input-group hspan">
 					<label for="name" class="input-group-addon">站台名稱</label>
-					<input type="text" class="form-control" id="name" name="name" placeholder="僅可使用符合 [a-z0-9_-] 字符">
+					<input type="text" class="form-control" id="name" name="name" placeholder="僅可使用符合 [a-z0-9_-] 字符" required>
+				</div>
+				<div class="input-group hspan">
+					<label for="project" class="input-group-addon">專案名稱</label>
+					<input type="text" class="form-control" id="project" name="project" placeholder="系統名稱; 僅可使用符合 [a-z0-9_-] 字符" required>
 				</div>
 				<div class="input-group hspan">
 					<label for="status" class="input-group-addon">站台狀態</label>
 					<select class="form-control" id="status" name="status">
 						<option value="null">註冊</option>
-						<option value="null">下線</option>
-						<option value="null">上線</option>
+						<option value="0">下線</option>
+						<option value="1">上線</option>
 					</select>
 				</div>
 
 				<div class="dynamic input-group hspan">
 					<label class="input-group-addon">綁定網域</label>
-					<input type="text" class="form-control" name="domain[]" placeholder="請輸入包含 http:// 或 https:// 的完整網址">
+					<select class="form-control" name="protocol[]" required>
+						<option value="">是否開啟 SSL 加密</option>
+						<option value="0">否</option>
+						<option value="1">是</option>
+					</select>
+					<input type="text" class="form-control" name="domain[]" placeholder="請輸入綁定的網域" required>
 				</div>
 
 			</div>
